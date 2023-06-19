f4cd75215dd684c20f57cc5d2d51685a8e78dbf2
Maviel My Segurog <eunhaismine@gmail.com>
2023-06-19 23:39:56 +0800

    FullStackWeb

---
+++ FullStackApp/_id.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>User</h1> <br>
+        <button>Back</button>
+        <h1>{{ user.name }}</h1>
+        <p>Course: {{ user.course }}</p>
+        <p>Is Active: {{ user.isActive ? 'Active' : 'Inactive' }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: null,
+        };
+    },
+    mounted() {
+        const userID = this.$route.params.id;
+        this.fetchUser(userID)
+    },
+    methods: {
+        fetchUser(userID) {
+            this.user = {
+                id: userID,
+                name:'Mabby Segurog',
+                course: 'BSIT',
+                isActive: true,
+            };
+        },
+        goBack() {
+            this.$$router.go(-1);
+        },
+    },
+};
+</script>
---
+++ FullStackApp/app.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>Users</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Course</th>
+                    <th>Is Active</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in users" :key="user.id">
+                    <td>{{ user.name }}</td>
+                    <td>{{ user.course }}</td>
+                    <td>{{ user.isActive ? 'Active' : 'Inactive'}}</td>
+                    <td>
+                        <a class="router-link-active router-link-exact-active" href="/_id.vue"></a>
+                        <NuxtLink to= "app/_id.vue">view</NuxtLink>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            users: [
+                {
+                    id: 1001,
+                    name:'Mabby Segurog',
+                    course: 'BSIT',
+                    isActive: true,
+                },
+                {
+                    id: 1002,
+                    name:'Caleb Fabila',
+                    course: 'BSIT',
+                    isActive: true,
+                },
+                {
+                    id: 1003,
+                    name:'Calby Haus',
+                    course: 'BSIT',
+                    isActive: false,
+                },
+            ],
+        };
+    },
+};
+</script>
